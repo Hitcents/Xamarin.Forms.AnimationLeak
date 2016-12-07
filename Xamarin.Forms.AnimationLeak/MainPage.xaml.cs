cc6893c90bc292387fcfa4c0587963f6235131a4
Jonathan Peppers <jonathan.peppers@hitcents.com>
2016-12-07 09:40:28 -0600

    Demo project showing the issue

---
+++ Xamarin.Forms.AnimationLeak/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Xamarin.Forms.AnimationLeak
+{
+    public partial class MainPage : ContentPage
+    {
+        private List<WeakReference> _references = new List<WeakReference>();
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_Clicked(object sender, EventArgs e)
+        {
+            LogWeakReferences();
+
+            //NOTE: if we just do this, there is no memory leak because of SpinnerView.OnParentSet()
+            //var view = new SpinningView();
+
+            //NOTE: unfortunately if you have a deep hierarchy in your app, more likely this would happen (the parent gets unparented instead of the child)
+            var view = new ContentView { Content = new SpinningView() };
+
+            _references.Add(new WeakReference(view));
+            _content.Content = view;
+        }
+
+        async void LogWeakReferences()
+        {
+            //Forces GC before we log the references
+            GC.Collect();
+            await Task.Delay(100);
+            GC.Collect();
+
+            Debug.WriteLine("Weak References: " + _references.Count);
+            for (int i = 0; i < _references.Count; i++)
+            {
+                Debug.WriteLine("IsAlive: " + _references[i].IsAlive);
+            }
+            Debug.WriteLine("---------------------------------------");
+        }
+    }
+}
---
+++ Xamarin.Forms.AnimationLeak/SpinningView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Xamarin.Forms.AnimationLeak
+{
+    public partial class SpinningView : ContentView
+    {
+        private string AnimationKey = "Spinning";
+
+        public SpinningView()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnParentSet()
+        {
+            base.OnParentSet();
+
+            if (Parent != null)
+            {
+                this.Animate(AnimationKey, v => _box.Rotation = v, 0d, 360d, repeat: () => true);
+            }
+            else
+            {
+                this.AbortAnimation(AnimationKey);
+            }
+        }
+    }
+}
